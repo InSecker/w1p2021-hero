dc0300a15ee260b4e73e1c35e48cfb3adca0fe24
Pierre Secher <pierre@MacBook-Pro-de-Pierre.local>
2019-05-26 11:03:10 +0200

    add analytics

--- app/app.js
+++ app/app.js
@@ -1,5 +1,11 @@
 import './assets/scss/styles.scss';
 import Vue from 'vue';
 import router from './router';
+import VueAnalytics from 'vue-analytics'
 
 new Vue({ router: router }).$mount('#root');
+
+Vue.use(VueAnalytics, {
+  id: 'UA-133491732-2'
+});
+
